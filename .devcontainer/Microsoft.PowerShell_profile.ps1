4099d5b5024535f72a562b1e9ae2a5038cd4fa4d
Martin Palkovic <martin@palkovic.org>
2023-02-20 08:29:48 -0400

    updated devcontainer.json and requirements.txt files

--- .devcontainer/Microsoft.PowerShell_profile.ps1
+++ .devcontainer/Microsoft.PowerShell_profile.ps1
@@ -9,3 +9,6 @@ $env:POSH_GIT_ENABLED=$true
 oh-my-posh init pwsh --config $env:POSH_THEME | Invoke-Expression
 
 # NOTE: You can override the above env var from the devcontainer.json "args" under the "build" key.
+
+# Aliases
+Set-Alias -Name ac -Value Add-Content
